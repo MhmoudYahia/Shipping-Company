d84ff3237476277e8deb78b6c65279ea71268e7c
Ahmed Osama Helmy <55411484+AhmedOsama198@users.noreply.github.com>
2022-04-26 00:48:07 +0200

    Testing file

--- Project_D_S/MainTest.cpp
+++ Project_D_S/MainTest.cpp
@@ -2,8 +2,10 @@
 #include"PriorityQueue.h"
 #include"Queue.h"
 #include"UIClass.h"
+#include "Company.h"
 using namespace std;
 int main() {
-	
+	Company* CompanyTest = new Company;
+	CompanyTest->Loading_File();
 	return 0;
 }
